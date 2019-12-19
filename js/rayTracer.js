9ba967ada0b0335e632d461245778e09a169dfdd
ZeHeyoa <30337756+ZeHeyoa@users.noreply.github.com>
2019-12-19 21:10:52 +0100

    Update rayTracer.js

--- js/rayTracer.js
+++ js/rayTracer.js
@@ -393,7 +393,7 @@
 						pix[xy+2]=130;
 						continue;
 					}
-					pix[xy]=(pcr>1)?255:(pcr*255|100);
+					pix[xy]=(pcr>1)?255:(pcr*255|0);
 					pix[xy+1]=(pcg>1)?255:(pcg*255|0);
 					pix[xy+2]=(pcb>1)?255:(pcb*255|0);
 					pcr=pcg=pcb=0;
